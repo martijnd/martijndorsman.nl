827b5697f0a88c131da26407bac3df1f8e95f4e6
Martijn Dorsman <martijn.dorsman@gmail.com>
2018-04-15 23:18:09 +0200

    small fixes

--- js/main.js
+++ js/main.js
@@ -6,39 +6,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function background_gradient2(){
-
-    let colors = {
-        0: ['1'],
-        1: ['2'],
-        2: ['3'],
-        3: ['4'],
-        4: ['5'],
-        5: ['6'],
-        6: ['7'],
-        7: ['8'],
-        8: ['9'],
-        9: ['0'],
-        10: ['A'],
-        11: ['B'],
-        12: ['C'],
-        13: ['D'],
-        14: ['E'],
-        15: ['F'],
-
-    };
-
-    let randomAngle = getRandomInt(360);
-    let color1 = colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)];
-    let color2 = colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)] + colors[getRandomInt(16)];
-    let style = 'linear-gradient(' + randomAngle + 'deg, #' + color1.toString() + ', #' + color2.toString() + ' 100%)';
-    console.log(style);
-
-    $('.fullpage').css('background', style);
-    $( "<style>.list li::before { background: " + style + " }</style>" ).appendTo( "head" );
-
-}
-
 function background_gradient(){
 
     let colors = {
