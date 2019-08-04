f872dae029920622045da38164b35a90d2465425
martijnd <martijn.dorsman@gmail.com>
2019-08-04 17:34:58 +0200

    Remove jQuery and refactored main.js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,55 @@
-$(() => {
-    background_gradient();
-    fetchData();
-    console.log("Beep boop");
+document.addEventListener("DOMContentLoaded", () => {
+  background_gradient();
+  fetchData();
+  console.log("Beep boop");
 });
 
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+const $ = selector => document.querySelector(selector);
 
 const repeat = count => {
-    let strings = [];
-    while (strings.length < count) {
-        strings.push(getRandomInt(16).toString(16));
-    }
-    return strings.join("");
+  let strings = [];
+  while (strings.length < count) {
+    strings.push(getRandomInt(16).toString(16));
+  }
+  return strings.join("");
 };
 
 const fetchData = () => {
-    const username = "Gastlyguy";
-    let keyTotal = 0;
-    let clickTotal = 0;
-    const start = moment()
-        .startOf("day")
-        .unix();
-    const end = moment().unix();
-    $.get(
-        `https://api.whatpulse.org/pulses.php?user=${username}&start=${start}&end=${end}&format=json`,
-        response => {
-            if (response.error) {
-                $("#current-keys").text("0");
-                $("#current-clicks").text("0");
-                return;
-            }
+  const username = "Gastlyguy";
+  let keyTotal = 0;
+  let clickTotal = 0;
+  const start = moment()
+    .startOf("day")
+    .unix();
+  const end = moment().unix();
+  fetch(
+    `https://api.whatpulse.org/pulses.php?user=${username}\
+    &start=${start}&end=${end}&format=json`
+  )
+    .then(res => res.json())
+    .then(response => {
+      if (response.error) {
+        $("#current-keys").innerHTML = "0";
+        $("#current-clicks").innerHTML = "0";
+        return;
+      }
 
-            Object.values(response).forEach(pulse => {
-                keyTotal += +pulse.Keys;
-                clickTotal += +pulse.Clicks;
-            });
-            $("#current-keys").text(keyTotal);
-            $("#current-clicks").text(clickTotal);
-        }
-    );
+      Object.values(response).forEach(pulse => {
+        keyTotal += +pulse.Keys;
+        clickTotal += +pulse.Clicks;
+      });
+      $("#current-keys").innerHTML = keyTotal;
+      $("#current-clicks").innerHTML = clickTotal;
+    });
 };
 
 const background_gradient = () => {
-    const background = `linear-gradient(${getRandomInt(360)}deg, \
+  const background = `linear-gradient(${getRandomInt(360)}deg, \
     #${repeat(6)}, #${repeat(6)} 100%) `;
-    $(
-        `<style>.list li::before { background: ${background}; } a:after { background: ${background};} .fullpage { background: ${background}; }</style>`
-    ).appendTo("head");
+
+  const styleElement = document.createElement("style");
+  const style = `.list li::before { background: ${background}; } a:after { background: ${background};} .fullpage { background: ${background}; }`;
+  styleElement.appendChild(document.createTextNode(style));
+  document.head.appendChild(styleElement);
 };
