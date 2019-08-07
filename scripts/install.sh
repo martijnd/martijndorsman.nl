1bfbb0a23cb333a430f558e52f3cf107b0fc0f15
martijnd <martijn.dorsman@gmail.com>
2019-08-07 23:37:24 +0200

    Fixed private key

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 set -x
 
 #import the deployment key
-openssl aes-256-cbc -K $encrypted_67d9941d7096_key -iv $encrypted_67d9941d7096_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_120bdf80dce1_key -iv $encrypted_120bdf80dce1_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
