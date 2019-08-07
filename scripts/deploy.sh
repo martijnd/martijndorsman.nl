744097c4650424b4bb0b43b88cb1f0f6ded70960
martijnd <martijn.dorsman@gmail.com>
2019-08-07 23:09:56 +0200

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-if [$TRAVIS_BRANCH == 'master'] ; then
+if [ $TRAVIS_BRANCH == 'master' ] ; then
     rsync -az -e "ssh -o StrictHostKeyChecking=no" ./public/ deploy@167.71.73.126:/var/www/martijnd/
 else
     echo "Not deploying, since this branch isn't master"
