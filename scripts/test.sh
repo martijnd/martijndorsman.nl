93a6052294fbd754c5b23a76b1d33d91b5456e33
martijnd <martijn.dorsman@gmail.com>
2019-08-07 23:05:46 +0200

    Fix 403

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-timeout 30s bundle exec htmlproofer public --only-4xx --external_only --check-html --check-favicon --allow-hash-href --http-status-ignore 429
+timeout 30s bundle exec htmlproofer public --only-4xx --external_only --check-html --check-favicon --allow-hash-href --http-status-ignore "429,403"
