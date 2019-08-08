83baae74b8b19ca1d2a35f181c2446ea498d0d66
martijnd <martijn.dorsman@gmail.com>
2019-08-08 20:42:53 +0200

    Test 123

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 set -x
 
 #import the deployment key
-openssl aes-256-cbc -K $encrypted_1822c372ce6f_key -iv $encrypted_1822c372ce6f_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_120bdf80dce1_key -iv $encrypted_120bdf80dce1_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc
 chmod 600 deploy-key
 ls ~/.ssh
