7c018bdbd7fbd1cc1b6739f01dc7b4523c2e09f5
Martijn Dorsman <mdorsman@flaire.nl>
2019-08-08 11:11:07 +0200

    Add ls

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ set -x
 openssl aes-256-cbc -K $encrypted_1822c372ce6f_key -iv $encrypted_1822c372ce6f_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc
 chmod 600 deploy-key
+ls ~/.ssh
 mv deploy-key ~/.ssh/id_rsa
 
 npm install uglify-es -g
