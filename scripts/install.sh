d2838900a57ee3e11c643199db43d09e99da6aff
Martijn Dorsman <mdorsman@flaire.nl>
2019-08-08 11:37:10 +0200

    Test

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,11 @@
 set -x
 
 #import the deployment key
-openssl aes-256-cbc -K $encrypted_1822c372ce6f_key -iv $encrypted_1822c372ce6f_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_1822c372ce6f_key -iv $encrypted_1822c372ce6f_iv -in deploy-key2.enc -out deploy-key2 -d
 rm deploy-key.enc
-chmod 600 deploy-key
+chmod 600 deploy-key2
 ls ~/.ssh
-mv deploy-key ~/.ssh/id_rsa
+mv deploy-key2 ~/.ssh/id_rsa
 ls ~/.ssh
 
 npm install uglify-es -g
