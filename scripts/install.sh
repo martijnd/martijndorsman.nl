e75ecf91480901a51b355b50a161f01b0818ab3f
Martijn Dorsman <mdorsman@flaire.nl>
2019-08-08 10:34:25 +0200

    Changed endpoint

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 set -x
 
 #import the deployment key
+
 openssl aes-256-cbc -K $encrypted_1822c372ce6f_key -iv $encrypted_1822c372ce6f_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc
 chmod 600 deploy-key
